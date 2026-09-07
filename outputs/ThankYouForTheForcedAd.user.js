06a11d2de4ec3aebdbf67108f87c64f624324266
NellPoi <nellpoi@icloud.com>
2026-09-07 15:22:12 +0800

    Declare MIT license in userscript metadata

--- outputs/ThankYouForTheForcedAd.user.js
+++ outputs/ThankYouForTheForcedAd.user.js
@@ -3,6 +3,7 @@
 // @namespace    https://github.com/yourname
 // @version      3.0
 // @description  打开 Extra Tools Pack 的 crack 页面时立即尝试关闭该标签页
+// @license       MIT
 // @match        https://www.extratoolspack.com/crack.php
 // @run-at       document-start
 // ==/UserScript==
